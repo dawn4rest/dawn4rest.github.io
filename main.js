5740ea3a5af3667f2b7d1b8af2f8f6b51089f9ce
dawn4rest <dawn4rest@gmail.com>
2023-05-27 22:42:30 +0900

    feat: Costomize swiper full page

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
 $(function(){
-  setInterval(function(){
-    if($('.mainbotBk').hasClass("swiper-slide-active") === true ) {
-      $(".mainfull_navi").addClass("bk");
-      $("header.head").css("border","none");
-      $(".logo_header").css("color","#000");
-      $("nav > ul > li > a").css("color","#000");
-      $("header .menu-icon .navicon").addClass("bk");
-    }else {
-      $(".mainfull_navi").removeClass("bk");
-      $("header.head").css("border-bottom","1px solid #fff");
-      $(".logo_header").css("color","#fff");
-      $("nav > ul > li > a").css("color","#fff");
-      $("header .menu-icon .navicon").removeClass("bk");
-    }
-  },500);
-
-  var ww = $(window).width();
   var mySwiper = undefined;
 
   function initSwiper() {
-    if (ww >= 640 && mySwiper == undefined) {
+    if ($(window).width() >= 640 && mySwiper == undefined) {
       mySwiper = new Swiper(".swiper-container", {
         speed: 500,
         direction: 'vertical',
@@ -36,12 +19,6 @@ $(function(){
               mySwiper.params.mousewheel.releaseOnEdges = false;
             });
           },
-          reachBeginning: function() {
-            setTimeout(function () {
-              mySwiper.params.touchReleaseOnEdges = true;
-              mySwiper.params.mousewheel.releaseOnEdges = true;
-            }, 500);
-          }
         }
       });
     } 
@@ -51,10 +28,7 @@ $(function(){
 
   $(window).on('resize', function () {
     setTimeout(function(){
-      ww = $(window).width();
       initSwiper();
       },15);
   });
-
-  $("header").addClass("head");
 });
